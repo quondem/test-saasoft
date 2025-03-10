dd0202a7146a2543fe062e42a4d83df4f7228345
quondem <shul-m@mail.ru>
2025-03-10 23:36:10 +0300

    first commit

---
+++ src/components/Account/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="account">
+    123
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+@import url(./index.scss);
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="accounts" />
+</template>
+
+<script lang="ts" setup>
+//
+</script>
